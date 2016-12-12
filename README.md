76d633f2e313c3d94d67c940590d22207ffdd4dc
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-12-12 19:47:03 +0900

    first import

---
+++ README.md
@@ -0,0 +1,23 @@
+# mackerel-gops
+
+Upload [https://mackerel.io/](Mackerel) Custom Metrics from [gops](https://github.com/google/gops)
+
+## Usage
+
+```
+$ mackerel-gops -p 12345 -name procname -service servicename
+```
+
+## Installation
+
+```
+$ go get github.com/mattn/mackerel-gops
+```
+
+## License
+
+MIT
+
+## Author
+
+Yasuhiro Matsumoto (a.k.a. mattn)
---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	names = map[string]string{
+		"alloc":         "bytes",
+		"sys":           "bytes",
+		"lookups":       "integer",
+		"mallocs":       "integer",
+		"frees":         "integer",
+		"heap_alloc":    "bytes",
+		"heap_sys":      "bytes",
+		"heap_idle":     "bytes",
+		"heap_in_use":   "bytes",
+		"heap_released": "bytes",
+		"heap_objects":  "integer",
+		"stack_in_use":  "bytes",
+		"stack_sys":     "bytes",
+	}
+	proc    = flag.Int("p", 0, "process id")
+	prefix  = flag.String("name", "", "prefix of keys for metrics")
+	service = flag.String("service", "", "service name")
+	sleep   = flag.Duration("sleep", 5*time.Second, "sleep")
+)
+
+type metric struct {
+	Name  string  `json:"name"`
+	Time  int64   `json:"time"`
+	Value float64 `json:"value"`
+	Unit  string  `json:"unit"`
+}
+
+func main() {
+	flag.Parse()
+
+	if *prefix == "" || *service == "" || *proc == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	os.Setenv("GODEBUG", "http2client=0")
+
+	for {
+		time.Sleep(*sleep)
+
+		b, err := exec.Command("gops", "memstats", "-p", fmt.Sprint(*proc)).CombinedOutput()
+		if err != nil {
+			log.Fatal(err)
+		}
+		var metrics []metric
+		for _, line := range strings.Split(string(b), "\n") {
+			tokens := strings.SplitN(line, ":", 2)
+			if len(tokens) != 2 {
+				continue
+			}
+			name := strings.Replace(tokens[0], "-", "_", -1)
+			unit, ok := names[name]
+			if !ok {
+				continue
+			}
+			val, _ := strconv.ParseFloat(strings.Split(strings.TrimSpace(tokens[1]), " ")[0], 64)
+			metrics = append(metrics, metric{Name: *prefix + "." + name, Time: time.Now().Unix(), Value: val, Unit: unit})
+		}
+
+		var buf bytes.Buffer
+		err = json.NewEncoder(&buf).Encode(metrics)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		log.Print(buf.String())
+		req, err := http.NewRequest("POST", "https://mackerel.io/api/v0/services/"+*service+"/tsdb", &buf)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		req.Header.Set("X-Api-Key", os.Getenv("MACKEREL_API_KEY"))
+		req.Header.Set("content-type", "application/json")
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Print(err)
+		}
+		resp.Body.Close()
+	}
+}
